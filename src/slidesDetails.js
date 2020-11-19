efe08dc58d743adef095a0af09590f51cb16911a
Dan Ilea <doflada@gmail.com>
2020-11-19 19:03:33 +0200

    responsive layout

--- src/slidesDetails.js
+++ src/slidesDetails.js
@@ -7,7 +7,7 @@ export const slidesDetails = [
   {
     id: 2,
     slideName: "./pictureSunset/2020m02.jpg",
-    title: "February 2020",
+    title: "February 2020 at 18:28",
   },
   {
     id: 3,
@@ -17,7 +17,7 @@ export const slidesDetails = [
   {
     id: 4,
     slideName: "./pictureSunset/2020m04.jpg",
-    title: "April 2020",
+    title: "April 2020 at 21:05",
   },
   {
     id: 5,
@@ -27,26 +27,31 @@ export const slidesDetails = [
   {
     id: 6,
     slideName: "./pictureSunset/2020m06.jpg",
-    title: "June 2020",
+    title: "June 2020 at 21:05",
   },
   {
     id: 7,
     slideName: "./pictureSunset/2020m07.jpg",
-    title: "July 2020",
+    title: "July 2020 at 20:47",
   },
   {
     id: 8,
     slideName: "./pictureSunset/2020m08.jpg",
-    title: "August 2020",
+    title: "August 2020 at 20:27",
   },
   {
     id: 9,
     slideName: "./pictureSunset/2020m09.jpg",
-    title: "September 2020",
+    title: "September 2020 at 19:00",
   },
   {
     id: 10,
     slideName: "./pictureSunset/2020m10.jpg",
-    title: "October 2020",
+    title: "October 2020 at 18:36",
+  },
+  {
+    id: 11,
+    slideName: "./pictureSunset/2020m11.jpg",
+    title: "November 2020 at 15:05",
   },
 ];
